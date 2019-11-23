f868061f73f266e0ffbaa7b46b126ad7afc2c8a0
deocksoo <dusol92@snu.ac.kr>
2019-11-23 18:00:04 +0900

    feat: vue.js 빌드 타깃 디렉터리 변경 & gradle 빌드시 vue.js 자동빌드

--- src/frontend/vue.config.js
+++ src/frontend/vue.config.js
@@ -1,4 +1,8 @@
+const path = require('path')
+const BASE_TARGET_DIR = "main/resources/static"
+
 module.exports = {
+    outputDir: path.resolve(__dirname, "../" + BASE_TARGET_DIR),
     devServer: {
         proxy: {
             '/api': {
