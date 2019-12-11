55d260f5d77ec584ed9bc49ca93a091663573ccc
deocksoo <dusol92@snu.ac.kr>
2019-12-11 20:52:16 +0900

    style: App.vue indent 정리

--- src/frontend/src/App.vue
+++ src/frontend/src/App.vue
@@ -1,28 +1,28 @@
 <template>
-    <el-container>
-        <el-header align="center"><h1>Woowahan Chess</h1></el-header>
-        <el-row>
-            <el-col :span="16">
-                <chess-board></chess-board>
-            </el-col>
-            <el-col :span="8">
-                <status-board></status-board>
-            </el-col>
-        </el-row>
-    </el-container>
+  <el-container>
+    <el-header align="center"><h1>Woowahan Chess</h1></el-header>
+    <el-row>
+      <el-col :span="16">
+        <chess-board></chess-board>
+      </el-col>
+      <el-col :span="8">
+        <status-board></status-board>
+      </el-col>
+    </el-row>
+  </el-container>
 </template>
 
 <script>
-    import ChessBoard from "./components/ChessBoard";
-    import StatusBoard from "./components/StatusBoard";
+  import ChessBoard from "./components/ChessBoard";
+  import StatusBoard from "./components/StatusBoard";
 
-    export default {
-        name: 'app',
-        components: {
-            ChessBoard,
-            StatusBoard
-        }
+  export default {
+    name: 'app',
+    components: {
+      ChessBoard,
+      StatusBoard
     }
+  }
 </script>
 
 <style>
