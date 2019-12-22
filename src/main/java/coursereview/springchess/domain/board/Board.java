9694d7335cc51f4215beeb0878a210e01d6f6c69
skygl <jupiter9501@gmail.com>
2019-12-22 19:47:16 +0900

    feat: Board 클래스 추가 및 이동할 수 있는 Position 을 구하는 로직 추가

--- src/main/java/coursereview/springchess/domain/board/Board.java
+++ src/main/java/coursereview/springchess/domain/board/Board.java
@@ -1,19 +1,84 @@
 package coursereview.springchess.domain.board;
 
+import coursereview.springchess.domain.common.Color;
 import coursereview.springchess.domain.piece.Piece;
+import coursereview.springchess.domain.piece.PieceInfo;
+import coursereview.springchess.domain.piece.Position;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
+import java.util.stream.Collectors;
+
+import static coursereview.springchess.domain.piece.Type.PAWN;
 
 public class Board {
 
-    private final List<Piece> pieces;
+    private static final Logger log = LoggerFactory.getLogger(Board.class);
+
+    private final Map<Position, PieceInfo> board;
 
     public Board(final List<Piece> pieces) {
-        this.pieces = pieces;
+        board = pieces.stream()
+                .collect(Collectors.toMap(Piece::getPosition, Piece::getPieceInfo));
     }
 
-    public List<Piece> getPieces() {
-        return new ArrayList<>(pieces);
+    public Map<Position, List<Position>> calculateMovablePositions(Color turn) {
+        Map<Position, List<Position>> movablePositionsMap = new HashMap<>();
+
+        List<Position> positionsWithTurn = getPositionsWithTurn(turn);
+
+        for (Position position : positionsWithTurn) {
+            List<List<Position>> candidatesForAllDirections = board.get(position).getMovableCandidatesFor(position);
+
+            List<Position> movablePositions = candidatesForAllDirections.stream()
+                    .map(candidatesForOneDirection -> calculateMovablePositionsForOneDirection(position, candidatesForOneDirection))
+                    .flatMap(Collection::stream)
+                    .collect(Collectors.toList());
+
+            movablePositionsMap.put(position, movablePositions);
+        }
+
+        return movablePositionsMap;
+    }
+
+    private List<Position> getPositionsWithTurn(final Color turn) {
+        return board.entrySet().stream()
+                .filter(entry -> entry.getValue().matchColor(turn))
+                .map(Map.Entry::getKey)
+                .collect(Collectors.toList());
+    }
+
+    private List<Position> calculateMovablePositionsForOneDirection(Position piecePosition, List<Position> candidatesForOneDirection) {
+        List<Position> movablePositions = new ArrayList<>();
+        for (Position candidate : candidatesForOneDirection) {
+            log.info("piecePosition {}", piecePosition);
+            log.info("candidate {}", candidate);
+            if (isBlank(candidate)) {
+                if (isPawnAttack(piecePosition, candidate)) {
+                    break;
+                }
+                movablePositions.add(candidate);
+                continue;
+            }
+            if (isSameColor(piecePosition, candidate)) {
+                break;
+            }
+            movablePositions.add(candidate);
+            break;
+        }
+        return movablePositions;
+    }
+
+    private boolean isBlank(final Position candidate) {
+        return !board.containsKey(candidate);
+    }
+
+    private boolean isPawnAttack(final Position piecePosition, final Position candidate) {
+        return board.get(piecePosition).matchType(PAWN) && !piecePosition.matchColumn(candidate);
+    }
+
+    private boolean isSameColor(final Position piecePosition, final Position candidate) {
+        return board.get(piecePosition).matchColor(board.get(candidate));
     }
 }
