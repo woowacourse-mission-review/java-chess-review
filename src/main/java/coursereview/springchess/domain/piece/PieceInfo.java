d89c20e35f0fa07a8d0f3aca3420756f1cd289bd
skygl <jupiter9501@gmail.com>
2019-12-22 19:44:39 +0900

    feat: 폰 벡터 구하는 로직 및 움직일 수 있는 Position 을 벡터 별로 구하는 로직 추가

--- src/main/java/coursereview/springchess/domain/piece/PieceInfo.java
+++ src/main/java/coursereview/springchess/domain/piece/PieceInfo.java
@@ -7,10 +7,20 @@ import lombok.ToString;
 import javax.persistence.Column;
 import javax.persistence.Convert;
 import javax.persistence.Embeddable;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static coursereview.springchess.domain.common.Color.WHITE;
+import static coursereview.springchess.domain.piece.RowPosition.SEVEN;
+import static coursereview.springchess.domain.piece.RowPosition.TWO;
+import static coursereview.springchess.domain.piece.Type.PAWN;
 
 @Embeddable
 @ToString
 public class PieceInfo {
+
+    private static final int PAWN_INITIAL_MOVE_DISTANCE = 2;
+    private static final int PAWN_DEFAULT_MOVE_DISTANCE = 1;
 
     @Column(name = "type", nullable = false)
     private Type type;
@@ -33,5 +43,35 @@ public class PieceInfo {
 
     public Color getColor() {
         return color;
+    }
+
+    public boolean matchColor(final Color color) {
+        return this.color.equals(color);
+    }
+
+    public List<List<Position>> getMovableCandidatesFor(final Position position) {
+        List<Vector> vectors = type.equals(PAWN) ? getMovableVectorsOfPawn(position) : type.getVectors();
+        return vectors.stream()
+                .map(vector -> vector.getMovablePositions(position))
+                .collect(Collectors.toList());
+    }
+
+    private List<Vector> getMovableVectorsOfPawn(final Position position) {
+        List<Vector> vectors = Direction.valueOfPawnAttack(color).stream()
+                .map(direction -> new Vector(direction, PAWN_DEFAULT_MOVE_DISTANCE))
+                .collect(Collectors.toList());
+
+        int distance = position.matchRow(color.equals(WHITE) ? TWO : SEVEN) ? PAWN_INITIAL_MOVE_DISTANCE : PAWN_DEFAULT_MOVE_DISTANCE;
+        vectors.add(new Vector(Direction.valueOfPawnMove(color), distance));
+
+        return vectors;
+    }
+
+    public boolean matchColor(final PieceInfo pieceInfo) {
+        return this.color.equals(pieceInfo.color);
+    }
+
+    public boolean matchType(final Type type) {
+        return this.type.equals(type);
     }
 }
