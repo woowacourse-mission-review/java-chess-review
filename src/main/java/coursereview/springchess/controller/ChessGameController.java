c649026e84d2519d14f1cb3de2d4db927706d393
skygl <jupiter9501@gmail.com>
2019-12-22 13:01:37 +0900

    feat: ChessGameController 추가

---
+++ src/main/java/coursereview/springchess/controller/ChessGameController.java
@@ -0,0 +1,23 @@
+package coursereview.springchess.controller;
+
+import coursereview.springchess.service.ChessGameService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ChessGameController {
+
+    private ChessGameService chessGameService;
+
+    @Autowired
+    public ChessGameController(final ChessGameService chessGameService) {
+        this.chessGameService = chessGameService;
+    }
+
+    @GetMapping("/api/initialized-board")
+    public ResponseEntity initializeBoard() {
+        return ResponseEntity.ok(chessGameService.initialize());
+    }
+}
