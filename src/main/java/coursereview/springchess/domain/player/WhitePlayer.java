98ba0e03993b06d1a6197a641edd20180f545a2b
wbluke <pwb0127@naver.com>
2019-12-23 21:22:55 +0900

    feat: WhitePlayer, BlackPlayer 초기화 로직 추가

--- src/main/java/coursereview/springchess/domain/player/WhitePlayer.java
+++ src/main/java/coursereview/springchess/domain/player/WhitePlayer.java
@@ -1,7 +1,7 @@
 package coursereview.springchess.domain.player;
 
-import coursereview.springchess.domain.chesspiece.ChessPiece;
-import coursereview.springchess.domain.chesspiece.Rook;
+import coursereview.springchess.domain.chesspiece.*;
+import coursereview.springchess.domain.chesspiece.pawn.WhitePawn;
 import coursereview.springchess.domain.position.ChessPosition;
 
 import java.util.Map;
@@ -10,17 +10,17 @@ public class WhitePlayer extends ChessPlayer {
 
     public WhitePlayer() {
         pieces.put(ChessPosition.A1, new Rook());
-//        pieces.put(ChessPosition.B1, new Knight());
-//        pieces.put(ChessPosition.C1, new Bishop());
-//        pieces.put(ChessPosition.D1, new Queen());
-//        pieces.put(ChessPosition.E1, new King());
-//        pieces.put(ChessPosition.F1, new Bishop());
-//        pieces.put(ChessPosition.G1, new Knight());
+        pieces.put(ChessPosition.B1, new Knight());
+        pieces.put(ChessPosition.C1, new Bishop());
+        pieces.put(ChessPosition.D1, new Queen());
+        pieces.put(ChessPosition.E1, new King());
+        pieces.put(ChessPosition.F1, new Bishop());
+        pieces.put(ChessPosition.G1, new Knight());
         pieces.put(ChessPosition.H1, new Rook());
 
         for (char i = 'A'; i <= 'H'; i++) {
-//            String position = String.valueOf(i) + 2;
-//            pieces.put(ChessPosition.find(position), new WhitePawn());
+            String position = String.valueOf(i) + 2;
+            pieces.put(ChessPosition.find(position), new WhitePawn());
         }
     }
 
